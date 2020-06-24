8ab317258f773cf42815c55a77042fcbf0e2501e
DEEPANKAR-SIDHARTH <65945129+DEEPANKAR-SIDHARTH@users.noreply.github.com>
2020-06-24 11:05:57 +0530

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+echo $red 
+echo "This  Script is to install all Basic additional packages & updates."
+echo $white
+echo "╭━━━━╮ "
+echo "┃ ● ══  ┃ " 
+echo "┃███████┃ "
+echo "┃███████┃ "
+echo "┃███████┃ "
+echo "┃███████┃ "
+echo "┃███████┃ "
+echo "┃███████┃ "
+echo "┃███████┃ "
+echo "┃███████┃ "
+echo "┃  🄾    ┃"
+echo $red
+echo " Wait for sometime "
+apt-get update -y                      
+apt install python -y           
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+echo $green
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛ WeSoDev.Tech ÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+echo $blue
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛ WeSoDev.Tech ÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+echo $white
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛ WeSoDev.Tech ÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+echo $red
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛ WeSoDev.Tech ÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+echo $green
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛ WeSoDev.Tech ÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+echo $white
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛ WeSoDev.Tech ÛÛÛÛÛÛÛÛ"
+echo "ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ"
+echo "Have fun Now u r ready for New things"
